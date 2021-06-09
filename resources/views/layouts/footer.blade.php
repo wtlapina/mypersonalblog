7b4c83955db8255669c694ea5984e23c99008866
wtlapina <walter_lapina@ligph.com>
2021-06-09 16:39:55 +0800

    Inital Commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,94 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-100">
+                <li class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Blog
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Login
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Register
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Find Us
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-100">
+                <li class="pb-1">
+                    <a href="/">
+                        What we do
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Address
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Phone
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Latest posts
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-100">
+                <li class="pb-1">
+                    <a href="/">
+                        Post 1
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Post 2
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Post 3
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Post 4
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <p class="w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright 2021 Code With Waltz. All Rights Reserved
+    </p>
+</footer>
