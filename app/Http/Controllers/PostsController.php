7674999fd06fe16167df1c907281b04da9d92914
wtlapina <walter_lapina@ligph.com>
2021-06-18 16:59:10 +0800

    Added middleware contructor in PostController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,12 @@ use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class PostsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
