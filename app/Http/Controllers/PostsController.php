59bf5b484bce3513c18e5058a7501464f770a5ab
wtlapina <walter_lapina@ligph.com>
2021-06-18 16:42:50 +0800

    Added Edit page and Update function

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -75,33 +75,47 @@ class PostsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($slug)
     {
-        //
+        return view('blog.edit')
+            ->with('post', Post::where('slug', $slug)->first());
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $slug)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        Post::where('slug', $slug)
+            ->update([
+                'title' => $request->input('title'),
+                'description' => $request->input('description'),
+                'slug' => $slug
+            ]);
+
+        return redirect('/blog')
+            ->with('message', 'Your post has been updated!');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($slug)
     {
         //
     }
---
+++ resources/views/blog/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="w-4/5 m-auto text-center">
+    <div class="py-15">
+        <h1 class="text-6xl">
+            Update Post
+        </h1>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="w-4/5 m-auto text-center">
+        <ul>
+            @foreach($errors->all() as $error)
+                <li class="text-gray-50 bg-red-700 py-2">
+                    {{$error}}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<div class="w-4/5 m-auto pt-20">
+    <form 
+        action="/blog/{{$post->slug}}"
+        method="POST"
+        enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+
+        <input 
+            type="text"
+            name="title"
+            value="{{$post->title}}"
+            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+
+        <textarea 
+            name="description"
+            class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none">{{$post->description}}</textarea>
+
+        <button
+            type="submit"
+            class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+            Submit Post
+        </button>
+    </form>
+</div>
+
+@endsection
